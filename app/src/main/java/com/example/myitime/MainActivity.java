9a8b9f03a96d2fb64ae63bcf632b11a1a8f90143
Uzumaki1010 <13616919952@163.com>
2019-11-16 21:02:54 +0800

    添加了一个加号按钮。

--- app/src/main/java/com/example/myitime/MainActivity.java
+++ app/src/main/java/com/example/myitime/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.myitime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button floatingActionAddNewButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionAddNewButton=(Button)findViewById(R.id.floating_action_addnew_button);
     }
 }
