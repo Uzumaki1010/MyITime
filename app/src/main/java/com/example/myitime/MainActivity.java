0345f5a62887c1a357c5e0f8be2b4ec5af7989d1
Uzumaki1010 <13616919952@163.com>
2019-11-16 21:14:07 +0800

    修改了之前的错误，引用了design包

--- app/src/main/java/com/example/myitime/MainActivity.java
+++ app/src/main/java/com/example/myitime/MainActivity.java
@@ -5,14 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button floatingActionAddNewButton;
+    private FloatingActionButton floatingActionButtonAddNew;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        floatingActionAddNewButton=(Button)findViewById(R.id.floating_action_addnew_button);
+        floatingActionButtonAddNew=(FloatingActionButton)findViewById(R.id.floating_action_addnew_button);
     }
 }
